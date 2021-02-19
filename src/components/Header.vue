6391354a35ebfcda2756b178512af54d0d9616d7
booldook <booldook@gmail.com>
2021-02-19 11:30:40 +0900

    ...

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+	header.header-wrapper
+		.logo
+			i.fa.fa-sun
+		ul.navi-wrapper
+			li.navi
+				router-link(to='/') HOME
+			li.navi
+				router-link(to='/daily') DAILY
+			li.navi
+				router-link(to='/5days') 5DAYS
+</template>
+<script>
+export default {
+	name: 'Header',
+}
+</script>
+<style lang="scss" scoped>
+	
+</style>
