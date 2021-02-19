05c530333aaef61e259e66c689ce709a6426b832
booldook <booldook@gmail.com>
2021-02-19 11:13:12 +0900

    ...

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import { BootstrapVue } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-vue/dist/bootstrap-vue.min.css'
+
+Vue.use(BootstrapVue)
+
 Vue.config.productionTip = false
 
 new Vue({
